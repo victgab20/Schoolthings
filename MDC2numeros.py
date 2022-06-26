ed88595c5c7a62d171132615513e339be3b09cf3
Victor Gabriel <106008209+victgab20@users.noreply.github.com>
2022-06-26 13:43:17 -0300

    Create MDC2numeros.py

---
+++ MDC2numeros.py
@@ -0,0 +1,8 @@
+# Faça um programa,dado dois dados inteiros, positivos
+# e distinto x e y,calcular o mdc de x e y
+
+from math import gcd
+x = int(input('Digite o primeiro valor de x '))
+y = int(input('Digite o segundo valor de y '))
+gcd(8, 9)
+print('O MDC(', x,',', y,') =',(gcd(x, y)))
